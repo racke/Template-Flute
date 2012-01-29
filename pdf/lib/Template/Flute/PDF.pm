3e8a2e3b2b496a48a2c1575eefd0d6a968d4372d
Stefan Hornburg (Racke) <racke@linuxia.de>
2012-01-29 14:59:12 +0100

    Remove calculation of correction for rotated text from Template::Flute::PDF class.

--- pdf/lib/Template/Flute/PDF.pm
+++ pdf/lib/Template/Flute/PDF.pm
@@ -626,57 +626,6 @@ sub calculate {
 	else {
 		$height = $lines * $specs->{size};
 	}
-	
-	if (exists $specs->{props}->{rotate}) {
-	    my ($rotate, $radian, $new_height, $new_width);
-
-	    # rotation works other way around compared to CSS
-	    $rotate = 360 - $specs->{props}->{rotate};
-
-	    # determine new width and height of box used for rotated text
-	    $radian = deg2rad($rotate);
-
-	    $new_width = abs($max_width * cos($radian)) + abs($height * sin($radian));
-	    $new_height = abs($max_width * sin($radian)) + abs($height * cos($radian));
-	    
-	    my $corr_w = abs($max_width * cos($radian)) + abs($height * sin($radian));
-	    my $corr_h = $max_width * sin($radian) + $height * cos($radian);
-
-	    $specs->{props}->{correction} = {width => abs($height * sin($radian)), height => - abs($max_width * cos($radian))};
-	    $specs->{props}->{correction} = {width => 0, height => 0};
-
-	    # width correction
-	    if ($rotate <= 90) {
-		$specs->{props}->{correction}->{width} = $height * sin($radian);
-	    }
-	    elsif ($rotate < 180) {
-		$specs->{props}->{correction}->{width} = abs($max_width * cos($radian)) 
-		    + $height * 0.5 * sin($radian);
-            }
-	    elsif ($rotate < 270) {
-		$specs->{props}->{correction}->{width} = abs($max_width * cos($radian)) ;
-	    }
-	    elsif ($rotate) {
-		$specs->{props}->{correction}->{width} = $height + $height * sin($radian); # + $height * sin($radian);
-            }
-
-	    # height correction
-	    if ($rotate < 90) {
-		$specs->{props}->{correction}->{height} = $height * sin($radian) - $max_width * sin($radian);
-	    }
-	    elsif ($rotate <= 180) {
-		$specs->{props}->{correction}->{height} = $height - $max_width * sin($radian);
-	    }
-	    elsif ($rotate <= 270) {
-		$specs->{props}->{correction}->{height} = abs($height * sin($radian));
-	    }
-	    elsif ($rotate < 360) {
-		$specs->{props}->{correction}->{height} = - $height * sin($radian);
-            }
-
-	    $max_width = $new_width;
-	    $height = $new_height;
-        }
 
 	# adjust to fixed width
 	if ($avail_width) {
@@ -812,8 +761,7 @@ sub textbox {
 	if (length($boxtext) && $boxtext =~ /\S/) {
 	    # try different approach
 	    if (exists $props->{rotate}) {
-		$txeng->translate($parms{x} + $props->{correction}->{width}, 
-				  $parms{y} + $props->{correction}->{height},);
+		$txeng->translate($parms{x}, $parms{y});
 		$txeng->transform_rel(-rotate => 360 - $props->{rotate});
 	    }
 	    else {
