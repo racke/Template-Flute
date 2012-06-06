7964825681e5ae98179aa977a6d60d7986ec80e2
Stefan Hornburg (Racke) <racke@linuxia.de>
2012-06-06 18:26:53 +0200

    Bump up version number.

--- lib/Template/Flute.pm
+++ lib/Template/Flute.pm
@@ -14,11 +14,11 @@ Template::Flute - Modern designer-friendly HTML templating Engine
 
 =head1 VERSION
 
-Version 0.0030
+Version 0.0040
 
 =cut
 
-our $VERSION = '0.0030';
+our $VERSION = '0.0040';
 
 =head1 SYNOPSIS
 
