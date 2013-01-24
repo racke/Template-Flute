9b67b8ae037ddd902924b86cccf4722c6fe1249b
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-01-24 11:13:01 +0100

    Update copyright years.

--- lib/Template/Flute/Specification.pm
+++ lib/Template/Flute/Specification.pm
@@ -562,7 +562,7 @@ the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Template-F
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2010-2012 Stefan Hornburg (Racke).
+Copyright 2010-2013 Stefan Hornburg (Racke).
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
