4c4c8b13245c77e3895dedaa344a9df10509239f
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-09-17 10:06:27 +0200

    Add section about "Simple operators" to the documentation.

--- lib/Template/Flute.pm
+++ lib/Template/Flute.pm
@@ -1028,6 +1028,32 @@ is usually more convenient.
 
 =back
 
+=head1 SIMPLE OPERATORS
+
+=head2 append
+
+Appends the value to the text inside a HTML element or to an attribute
+if C<target> has been specified. This can be used in C<value> and C<param>
+specification elements.
+
+The example shows how to add a HTML class to elements in a list:
+
+HTML:
+
+    <ul class="nav-sub">
+        <li class="category"><a href="" class="catname">Medicine</a></li>
+    </ul>
+
+XML:
+
+    <specification>
+        <list name="category" iterator="categories">
+            <param name="name" class="catname"/>
+            <param name="catname" field="uri" target="href"/>
+            <param name="css" class="catname" target="class" op="append" joiner=" "/>
+        </list>
+    </specification>
+
 =head1 CONDITIONALS
 
 =head2 Display image only if present
