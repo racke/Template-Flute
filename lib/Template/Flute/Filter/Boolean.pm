d1a0d602407b7653d6c28833961f4f21ca90ad3c
Stefan Hornburg (Racke) <racke@linuxia.de>
2013-11-20 13:37:27 +0100

    Add boilerplate documentation to Template::Flute::Filter::Boolean.

--- lib/Template/Flute/Filter/Boolean.pm
+++ lib/Template/Flute/Filter/Boolean.pm
@@ -5,6 +5,20 @@ use warnings;
 
 use base 'Template::Flute::Filter';
 
+=head1 NAME
+
+Template::Flute::Filter::Boolean
+
+=head1 DESCRIPTION
+
+Boolean filter.
+
+=head1 METHODS
+
+=head2 filter
+
+=cut
+
 sub filter {
     my ($self, $value, %args) = @_;
    	if($value and lc($value) ne 'false'){
@@ -14,5 +28,21 @@ sub filter {
    		return undef;
    	}
 }
- 
+
+=head1 AUTHOR
+
+Grega Pompe <grega.pompe@informa.si>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Grega Pompe <grega.pompe@informa.si>
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
 1;
