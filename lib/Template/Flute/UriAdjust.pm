6305bd0ca44a9bf860fee66e0f15e238e5c5d4b4
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-03-09 15:26:08 +0100

    Add documentaton to UriAdjust module.

--- lib/Template/Flute/UriAdjust.pm
+++ lib/Template/Flute/UriAdjust.pm
@@ -9,20 +9,63 @@ use URI::Escape (qw/uri_unescape/);
 
 use Moo;
 
+=head1 NAME
+
+Template::Flute::UriAdjust - URI adjust class for Template::Flute
+
+=head1 SYNOPSIS
+
+    $new_path = Template::Flute::UriAdjust->new(
+                   uri => 'test',
+                   adjust => '/t/',
+                );
+
+=head1 DESCRIPTION
+
+Adjusts relative URIs to a base path.
+
+=head1 ATTRIBUTES
+
+=head2 adjust
+
+Base path.
+
+=cut
+
 has adjust => (
     is => 'rw',
     required => 1,
 );
+
+=head2 uri
+
+URI to be adjusted.
+
+=cut
 
 has uri => (
     is => 'rw',
     required => 1,
 );
 
+=head2 scheme
+
+URI scheme (defaults to C<http>).
+
+=cut
+
 has scheme => (
     is => 'rw',
     default => 'http',
 );
+
+=head1 METHODS
+
+=head2 result
+
+Returns new URI if it has adjusted. Otherwise it returns undef.
+
+=cut
 
 sub result {
     my ($self) = @_;
@@ -61,5 +104,20 @@ sub result {
     return;
 };
 
+=head1 AUTHOR
+
+Stefan Hornburg (Racke), <racke@linuxia.de>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Stefan Hornburg (Racke) <racke@linuxia.de>.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
 
 1;
