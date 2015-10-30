bdcc2c4ca43d341b5b4586fa0a2bec70db90f768
Stefan Hornburg (Racke) <racke@linuxia.de>
2015-10-30 11:06:23 +0100

    Add role for Twig elements.

---
+++ lib/Template/Flute/Role/Elements.pm
@@ -0,0 +1,18 @@
+package Template::Flute::Role::Elements;
+
+use Moo::Role;
+use Types::Standard qw/ArrayRef InstanceOf/;
+
+=head2 elts
+
+List of twig elements.
+
+=cut
+
+has elts => (
+    is => 'ro',
+    isa => ArrayRef [ InstanceOf ['XML::Twig::Elt'] ],
+    default => sub {[]},
+);
+
+1;
