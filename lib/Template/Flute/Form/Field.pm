250ed9afbe7dd2559011b9d2954c359895a416f7
Stefan Hornburg (Racke) <racke@linuxia.de>
2015-10-30 11:08:55 +0100

    Add Form::Field class.

---
+++ lib/Template/Flute/Form/Field.pm
@@ -0,0 +1,54 @@
+package Template::Flute::Form::Field;
+
+use Moo;
+use Types::Standard qw/ArrayRef HashRef InstanceOf/;
+
+=head1 NAME
+
+Template::Flute::Form::Field - Form object for Template::Flute templates.
+
+=head1 ATTRIBUTES
+
+=head2 type
+
+Type of form field.
+
+=cut
+
+has type => (
+    is => 'ro',
+);
+
+=head2 name
+
+Name of form field.
+
+=cut
+
+has name => (
+    is => 'ro',
+);
+
+=head2 iterator
+
+Name of iterator for this form field (if any).
+
+=cut
+
+has iterator => (
+    is => 'ro',
+);
+
+=head2 elts
+
+List of twig elements.
+
+=cut
+
+has elts => (
+    is => 'ro',
+    isa => ArrayRef [ InstanceOf ['XML::Twig::Elt'] ],
+    weak_ref => 1,
+);
+
+1;
