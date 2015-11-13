c3ed148929a3ba99a3ccf8e303cc6ac95efa4103
Stefan Hornburg (Racke) <racke@linuxia.de>
2015-11-13 11:42:50 +0100

    Add specification_node attribute to Elements role.

--- lib/Template/Flute/Role/Elements.pm
+++ lib/Template/Flute/Role/Elements.pm
@@ -5,6 +5,18 @@ use MooX::HandlesVia;
 use Types::Standard qw/ArrayRef InstanceOf/;
 use namespace::clean;
 
+=head2 specification_node
+
+Pointer to the respective node in the specification tree.
+
+=cut
+
+has specification_node => (
+    is => 'ro',
+    weak_ref => 1,
+    required => 1,
+);
+
 =head2 elts
 
 List of twig elements.
