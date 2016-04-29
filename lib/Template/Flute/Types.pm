b4d88e3b0b61fb2712b3f472dc1f6b02d4285e7a
Peter Mottram <peter@sysnix.com>
2016-04-29 14:45:25 +0200

    cleanup lazy builders in main class and add some more types

--- lib/Template/Flute/Types.pm
+++ lib/Template/Flute/Types.pm
@@ -15,18 +15,36 @@ use warnings;
 
 use Type::Library -base;
 use Type::Utils -all;
+use Sub::Quote 'quote_sub';
 
 BEGIN { extends "Types::Standard" }
 
 =head1 TYPES
 
-=head2 SpecificationFile
+=head2 HtmlParser
+
+An instance of L<Template::Flute::HTML>.
+
+=head2 Specification
+
+An instance of L<Template::Flute::Specification>.
+
+=head2 URI
+
+An instance of L<URI>.
 
 =cut
 
-declare 'SpecificationFile',
-  as 'Str',
-  where { -f $_ },
-  message { "Template::Flute specification file does not exist: $_" };
+declare 'HtmlParser',
+  as InstanceOf ['Template::Flute::HTML'];
+
+declare 'ReadableFilePath',
+  constraint => quote_sub q{ -e $_ && -r $_ };
+
+declare 'Specification',
+  as InstanceOf ['Template::Flute::Specification'];
+
+declare 'URI',
+  as InstanceOf ['URI'];
 
 1;
